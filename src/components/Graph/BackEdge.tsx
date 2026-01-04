46ca00e704a8a4867f5683cd485c2a5e914c40ef
Taiki Harada <haradataiki19990927@gmail.com>
2026-01-04 10:32:43 +0900

    feat: Generate `BackEdge` path using arcs and multi-segment cubic Bezier curves instead of a simple Bezier curve.

--- src/components/Graph/BackEdge.tsx
+++ src/components/Graph/BackEdge.tsx
@@ -8,20 +8,34 @@ const BackEdge = ({
     style = {},
     markerEnd,
 }: EdgeProps) => {
-    const offset = 200;
-
-    // Multi-segment Bezier was not bulgy enough.
-    // User requested separate points on src and dst sides.
-
-    // CP1: Right of source (sourceX + offset)
-    // CP2: Right of target (targetX + offset)
-    const path = [
-        `M ${sourceX} ${sourceY}`,
-        `C ${sourceX + offset} ${sourceY}`,
-        `${targetX + offset} ${targetY}`,
-        `${targetX} ${targetY}`,
-    ].join(' ');
-
+    // parameter
+    const upLength = 150;
+    const radiusX = 90;
+    const radiusY = 70;
+    const curveIntensity = upLength;
+    // source
+    const sourceArcEndX = sourceX + (radiusX * 2);
+    const sourceArcEndY = sourceY;
+    // target
+    const targetArcStartX = targetX + (radiusX * 2);
+    const targetArcStartY = targetY;
+    // 頂点
+    const topX = (sourceArcEndX + targetArcStartX) / 2;
+    const topY = ((sourceArcEndY - upLength) + (targetArcStartY + upLength)) / 2;
+    // 制御点
+    const cp1X = sourceArcEndX;
+    const cp1Y = sourceArcEndY - curveIntensity;
+    const cp2X = sourceArcEndX;
+    const cp2Y = sourceArcEndY - upLength;
+    const cp3X = targetArcStartX;
+    const cp3Y = targetArcStartY + curveIntensity;
+    const path = `
+        M ${sourceX} ${sourceY}
+        A ${radiusX} ${radiusY} 0 0 0 ${sourceArcEndX} ${sourceArcEndY}
+        C ${cp1X} ${cp1Y} ${cp2X} ${cp2Y} ${topX} ${topY}
+        S ${cp3X} ${cp3Y} ${targetArcStartX} ${targetArcStartY}
+        A ${radiusX} ${radiusY} 0 0 0 ${targetX} ${targetY}
+    `;
     return (
         <BaseEdge path={path} markerEnd={markerEnd} style={style} />
     );
